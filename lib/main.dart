f95342505d0a39c8c54bd0a552891b8ec211e1a4
Guilherme <guilherme.pablo.rezende@gmail.com>
2024-05-24 17:53:59 -0300

    Ajuste no projeto

--- lib/main.dart
+++ lib/main.dart
@@ -55,23 +55,47 @@ Este arquivo contém a definição do modelo de dados para o tema do aplicativo.
 Este arquivo contém a definição do modelo de dados para a calculadora.
 
 6.	void main() { - Define a função main, que é o ponto de entrada do aplicativo Flutter.
+
 7.	runApp( - Esta função inicia o aplicativo Flutter.
+
 8.	MultiProvider( - Este widget é usado para fornecer múltiplos objetos para todos os widgets filhos.
+
 9.	providers: [ - Define a lista de objetos a serem fornecidos.
+
 10.	ChangeNotifierProvider( - Este widget é usado para fornecer uma instância de ThemeModel para todos os widgets filhos.
+
 11.	create: (context) => ThemeModel(), - Cria uma nova instância de ThemeModel.
+
 12.	ChangeNotifierProvider( - Este widget é usado para fornecer uma instância de Calculator para todos os widgets filhos.
+
 13.	create: (context) => Calculator(), - Cria uma nova instância de Calculator.
+
 14.	child: MyApp(), - Define o widget filho do MultiProvider. Este é o widget que será renderizado abaixo do MultiProvider.
-15.	class MyApp extends StatelessWidget { - Define a classe MyApp, que é um widget sem estado. Um widget sem estado é um widget que descreve parte da interface do usuário que pode depender de configuração, mas não depende do estado em execução.
-16.	const MyApp({super.key}); - Este é o construtor para a classe MyApp. Ele aceita um argumento nomeado key que é passado para a superclasse StatelessWidget.
-17.	@override - Esta anotação indica que a função build está substituindo uma função com o mesmo nome na superclasse.
-18.	Widget build(BuildContext context) { - Define o método build, que retorna o widget que este widget apresentará.
-19.	final themeModel = Provider.of<ThemeModel>(context); - Obtém a instância de ThemeModel do Provider.
+
+15.	class MyApp extends StatelessWidget { - Define a classe MyApp, que é um widget sem estado. 
+Um widget sem estado é um widget que descreve parte da interface do usuário que pode depender 
+de configuração, mas não depende do estado em execução.
+
+16.	const MyApp({super.key}); - Este é o construtor para a classe MyApp. Ele aceita 
+um argumento nomeado key que é passado para a superclasse StatelessWidget.
+
+17.	@override - Esta anotação indica que a função build está substituindo uma 
+função com o mesmo nome na superclasse.
+
+18.	Widget build(BuildContext context) { - Define o método build, 
+que retorna o widget que este widget apresentará.
+
+19.	final themeModel = Provider.of<ThemeModel>(context); - 
+Obtém a instância de ThemeModel do Provider.
+
 20.	return MaterialApp( - Retorna um MaterialApp, que é o widget raiz do aplicativo.
+
 21.	debugShowCheckedModeBanner: false, - Remove a faixa de debug do canto superior direito.
+
 22.	title: 'Calculadora', - Define o título do aplicativo.
+
 23.	theme: themeModel.currentTheme, - Define o tema do aplicativo usando o ThemeModel.
+
 24.	home: const HomeView(), - Define a HomeView como a tela inicial do aplicativo.
 
 
